edecf10f72de863206799db7a91b3893b749c3fc
23-4960-427 Mon Vencie Medalla <monvencie.medalla@cit.edu>
2025-06-22 10:05:02 +0800

    New Database Online Hosting

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";  // or 127.0.0.1
-$username = "root";         // default for XAMPP
-$password = "";             // default is empty
-$dbname = "furmaps_db";     // your database name
+$servername = "sql213.infinityfree.com";  // or 127.0.0.1
+$username = "if0_39292121";         // default for XAMPP
+$password = "8pBwOSvhQp";             // default is empty
+$dbname = "if0_39292121_furmaps";     // your database name
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
